ff531fd04216b0df7fe880e46ea79bcddcbd7617
Thiago <tdsimao@gmail.com>
2014-11-10 20:09:14 -0200

    Implement a Singleton Class

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from src.tests.randomTest import *
 from src.tests.matrixTest import *
+from src.tests.singletonTest import *
 
 if __name__ == '__main__':
     unittest.main()
