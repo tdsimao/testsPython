bc824c0667a02189bd7ee9663828697402b008c9
Thiago <tdsimao@gmail.com>
2014-12-01 10:18:05 -0200

    Challenge 02 - Done

---
+++ src/pythonchallenge/challenge02.py
@@ -0,0 +1,15 @@
+
+f = open('data/challenge02.txt')
+text = f.readlines()
+d = {}
+for l in text:
+    for c in l:
+        if c not in d:
+            d[c] = 0
+        d[c] = d[c] + 1
+
+for l in text:
+    for c in l:
+        if d[c] < 10:
+            print c,
+#print d
