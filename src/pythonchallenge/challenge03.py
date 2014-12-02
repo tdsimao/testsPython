30ccaaf9cf5cbc0db81996e6e30e96198469b442
Thiago <tdsimao@gmail.com>
2014-12-02 13:10:12 -0200

    Update Challenge 03 solution

--- src/pythonchallenge/challenge03.py
+++ src/pythonchallenge/challenge03.py
@@ -1,15 +1,5 @@
-
+import re
 f = open('data/challenge03.txt')
-text = f.readlines()
-d = {}
-for l in text:
-    for c in l:
-        if c not in d:
-            d[c] = 0
-        d[c] = d[c] + 1
-
-for l in text:
-    for c in l:
-        if d[c] < 10:
-            print c,
-#print d
+text = f.read()
+pattern = ''.join(re.findall('[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]',text))
+print pattern
