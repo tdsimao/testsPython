e9613f9b558c1d40ace7df784e3c95603ef823f3
tdsimao <tdsimao@gmail.com>
2014-12-14 21:13:01 -0200

    Challenge 13 - Done

--- src/pythonchallenge/challenge13.py
+++ src/pythonchallenge/challenge13.py
@@ -1,2 +1,18 @@
-
+from xmlrpclib import ServerProxy
 # https://docs.python.org/2/library/xmlrpclib.html
+
+proxy = ServerProxy('http://www.pythonchallenge.com/pc/phonebook.php')
+
+## list methods
+#>>> proxy.system.listMethods()
+# ['phone', 'system.listMethods', 'system.methodHelp', 'system.methodSignature', 'system.multicall', 'system.getCapabilities']
+
+## get phone signature
+#>>> proxy.system.methodSignature('phone')
+#[['string', 'string']]
+
+# test phone method
+#>>> proxy.phone('teste')
+#'He is not the evil'
+
+print proxy.phone('Bert')
