7ec19722b1b026b6b29720e396211f8459779c38
Thiago <tdsimao@gmail.com>
2014-12-21 12:56:13 -0200

    Challenge 18 - First Step

--- src/pythonchallenge/challenge18.py
+++ src/pythonchallenge/challenge18.py
@@ -5,22 +5,26 @@ fileName = 'data/baloons.jpg'
 retrieve_file('http://www.pythonchallenge.com/pc/return/balloons.jpg',fileName)
 im = Image.open(fileName)
 (w,h) = im.size
-im_out = Image.new(im.mode,(w/2,h))
 
-bars = {}
+im1 = Image.new(im.mode,(w/2,h))
+im2 = Image.new(im.mode,(w/2,h))
+
 for y in range(h):
     for x in range(w/2):
-        p = [0]*3
-        for i in range(3):
-            p[i] = im.getpixel((x,y))[i] - im.getpixel((x+w/2,y))[i]
-        #print im.getpixel((x,y)), im.getpixel((x+w/2,y)), tuple(p)
-        #print p
-        im_out.putpixel((x,y),tuple(p))
-im_out.show()
+         im1.putpixel((x,y),im.getpixel((x,y)))
+         im2.putpixel((x,y),im.getpixel((x+w/2,y)))
+
 #im.show()
 #im_out.save('data/challenge-out_18.jpg')
 
 # first clue
 #http://www.pythonchallenge.com/pc/return/colors.html
 #be more precise. +light : -light
+
+#http://www.pythonchallenge.com/pc/return/brightness.html
+#<!-- maybe consider deltas.gz -->
+#http://www.pythonchallenge.com/pc/return/deltas.gz
+deltas = 'data/deltas.gz'
+retrieve_file('http://www.pythonchallenge.com/pc/return/deltas.gz',deltas)
+
 
