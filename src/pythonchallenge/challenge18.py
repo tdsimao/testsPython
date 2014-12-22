d25e4c778a8a60bf584761f3c2b7ff9da92be04e
Thiago <tdsimao@gmail.com>
2014-12-22 13:52:36 -0200

    Challenge 18 - Done

--- src/pythonchallenge/challenge18.py
+++ src/pythonchallenge/challenge18.py
@@ -1,5 +1,7 @@
 from lib.misc import retrieve_file
 from PIL import Image
+import gzip,difflib
+
 
 fileName = 'data/baloons.jpg'
 retrieve_file('http://www.pythonchallenge.com/pc/return/balloons.jpg',fileName)
@@ -14,8 +16,10 @@ for y in range(h):
          im1.putpixel((x,y),im.getpixel((x,y)))
          im2.putpixel((x,y),im.getpixel((x+w/2,y)))
 
-#im.show()
-#im_out.save('data/challenge-out_18.jpg')
+import ImageChops
+im_out = ImageChops.difference(im1,im2)
+im_out.show('Teste')
+im_out.save('data/challenge-out_18.jpg')
 
 # first clue
 #http://www.pythonchallenge.com/pc/return/colors.html
@@ -24,7 +28,29 @@ for y in range(h):
 #http://www.pythonchallenge.com/pc/return/brightness.html
 #<!-- maybe consider deltas.gz -->
 #http://www.pythonchallenge.com/pc/return/deltas.gz
+
 deltas = 'data/deltas.gz'
 retrieve_file('http://www.pythonchallenge.com/pc/return/deltas.gz',deltas)
+
+lines = gzip.open(deltas,'rb').read().split('\n')
+
+
+pairs = [ (l[:53], l[56:]) for l in lines]
+
+left = [pair[0] for pair in pairs]
+right = [pair[1] for pair in pairs]
+
+diff = list(difflib.ndiff(left, right))
+
+import codecs,re
+for c in '-+ ':
+    s = ''.join(filter(lambda l: l[0] == c , diff))
+    s = codecs.getdecoder('hex')(re.sub('[^0-9a-fA-F]', '', s))[0]
+    fileName = 'data/challenge18_%c.png' % c
+    f = open(fileName,'wb')
+    f.write(s)
+    f.close()
+    im = Image.open(fileName)
+    im.show()
 
 
